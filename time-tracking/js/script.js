f2c484d33b0d3313888a0ed2a4132e3f831f2dc0
Ahmed  Mohamed <102030194+AhmedMohamed05@users.noreply.github.com>
2023-05-25 18:56:43 +0300

    Update script.js

--- time-tracking/js/script.js
+++ time-tracking/js/script.js
@@ -38,7 +38,7 @@ function showData(arr) {
 }
 
 function getData() {
-  fetch("../data.json")
+  fetch("./data.json")
     .then((response) => response.json())
     .then((data) => showData(data));
 }
